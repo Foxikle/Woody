02ac4dcc5038253a987f7e44fbdb739e8f42aca4
FlameyosFlow <89988527+FlameyosSnowy@users.noreply.github.com>
2024-07-03 15:29:51 +0300

    Delete core directory

--- core/src/main/java/me/flame/menus/util/ItemResponse.java
+++
@@ -1,9 +0,0 @@
-package me.flame.menus.util;
-
-import org.bukkit.entity.Player;
-import org.bukkit.event.inventory.InventoryClickEvent;
-
-@FunctionalInterface
-public interface ItemResponse {
-    void execute(Player player, InventoryClickEvent event);
-}
